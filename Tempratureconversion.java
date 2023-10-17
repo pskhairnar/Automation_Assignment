0442b517b8a2ccd671b53ef4495cc3c30e060095
lenovo <lenovo@DESKTOP-TAUO9JV>
2023-10-17 22:20:23 +0530

    Temprature conversion program

---
+++ Tempratureconversion.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Tempratureconversion {
+	
+	public static void main(String[] args) {
+		
+		Scanner Scan =new Scanner(System.in);
+		
+		System.out.println("Enter Temprature in celcius:");
+		
+		double Tempraturecelcius = Scan.nextDouble();
+		
+		double tempraturfahrenheit = 1.8*Tempraturecelcius+32;
+		
+		System.out.println("Temprature in Fahrenheit:"+ " " + tempraturfahrenheit);
+		
+		int kelvin = (int) (Tempraturecelcius+273);
+		
+		System.out.println("Temprature in Kelvin:"+" "+ kelvin);
+		
+	}
+	
+	
+
+}
